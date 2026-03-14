6edb37bf068424d968f6708eedbb4303202524e2
Titipon Kaorobtham <atom99990@gmail.com>
2026-03-15 02:17:05 +0700

    Add @vercel/analytics integration

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,11 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import { inject } from '@vercel/analytics';
+
 
 const queryClient = new QueryClient();
-
+inject();
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
